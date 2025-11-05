8f3a4d22e7ae8bd108bea41bd13132fd0e7ab514
Manh1234-ne <manhnon16@gmail.com>
2025-11-05 12:59:24 +0700

    Lab

---
+++ lab2.js
@@ -0,0 +1,35 @@
+// // var - function scoped
+// function varExample() {
+//   if (true) {
+//     var x = 10;
+//   }
+//   console.log(x); // 10 - vẫn truy cập được
+// }
+
+// varExample();
+
+
+
+// Bài 1: Sửa lỗi scope 
+// Sửa đoạn code sau để hoạt động đúng
+for (let i = 0; i < 3; i++) {
+  setTimeout(function () {
+    console.log(i); // Hiện tại in ra 3,3,3
+  }, 100);
+}
+// Yêu cầu: Sửa để in ra 0,1,2
+
+
+
+// Bài 2: Sử dụng const đúng cách
+// Tạo một object student với const
+// Thêm thuộc tính điểm và in ra
+const student = {
+  name: "ThuyTien",
+  age: 20,
+};
+
+// Thực hiện các thao tác sau:
+// 1. Thêm thuộc tính grade với giá trị "A"
+// 2. Thay đổi tuổi thành 21
+// 3. In ra toàn bộ thông tin student
