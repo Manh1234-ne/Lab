e06e616de49e87791d8f9a9498bc051f6148828d
Manh1234-ne <manhnon16@gmail.com>
2025-11-07 13:44:38 +0700

    lab3.js

--- lab3.js
+++ lab3.js
@@ -81,10 +81,12 @@ function mergeArrays(...arrays) {
 }
 console.log(mergeArrays([1, 2], [3, 4], [5]));
 
+
 function sumAll(...numbers) {
   return numbers.reduce((total, num) => total + num, 0);
 }
 console.log(sumAll(1, 2, 3, 4, 5));
+
 
 function createProduct(name = "Unknown Product", price = 0, quantity = 0) {
   return { name, price, quantity };
